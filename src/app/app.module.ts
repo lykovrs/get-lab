e2968d287845d6b7bc9f56fd43344b27b1072ae4
Roman Lykov <lykovrs@gmail.com>
2016-11-08 01:10:56 +0300

    Add base route states

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,47 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GreetingComponent } from './greeting/greeting.component';
+import { TextbookComponent } from './textbook/textbook.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/greeting',
+    pathMatch: 'full'
+  },
+  {
+    path: 'greeting',
+    component: GreetingComponent
+  },
+  {
+    path: 'textbook',
+    component: TextbookComponent
+  },
+  {
+    path: 'questionnaire',
+    component: QuestionnaireComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetingComponent,
+    TextbookComponent,
+    QuestionnaireComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [ RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
